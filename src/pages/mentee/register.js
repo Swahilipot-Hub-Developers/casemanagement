69d6ec18cbfd71f7ed1d7b7051d7cbb66435612e
ca <achinga.chris@gmail.com>
2024-01-18 01:06:24 +0300

    format-file

--- src/pages/mentee/register.js
+++ src/pages/mentee/register.js
@@ -15,39 +15,42 @@ const register = () => {
               </p>
             </div>
             <form className="row g-3 mt-2 position-relative z-index-9">
-  {/* Name */}
-  <div className="col-lg-6">
-    <label className="form-label">Name *</label>
-    <input type="text" className="form-control" aria-label="First name" />
-  </div>
-  {/* Email */}
-  <div className="col-lg-6">
-    <label className="form-label">Email *</label>
-    <input type="email" className="form-control" />
-  </div>
-  {/* Number */}
-  <div className="col-lg-12">
-    <label className="form-label">Phone Number *</label>
-    <input type="text" className="form-control" />
-  </div>
-  {/* Textarea */}
-  <div className="col-12">
-    <label className="form-label">Add Summary *</label>
-    <textarea
-      className="form-control"
-      rows={3}
-      spellCheck="false"
-      defaultValue={""}
-    />
-  </div>
-  {/* Button */}
-  <div className="col-12">
-    <button type="submit" className="btn btn-primary mb-0">
-      Submit form
-    </button>
-  </div>
-</form>
-
+              {/* Name */}
+              <div className="col-lg-6">
+                <label className="form-label">Name *</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  aria-label="First name"
+                />
+              </div>
+              {/* Email */}
+              <div className="col-lg-6">
+                <label className="form-label">Email *</label>
+                <input type="email" className="form-control" />
+              </div>
+              {/* Number */}
+              <div className="col-lg-12">
+                <label className="form-label">Phone Number *</label>
+                <input type="text" className="form-control" />
+              </div>
+              {/* Textarea */}
+              <div className="col-12">
+                <label className="form-label">Add Summary *</label>
+                <textarea
+                  className="form-control"
+                  rows={3}
+                  spellCheck="false"
+                  defaultValue={""}
+                />
+              </div>
+              {/* Button */}
+              <div className="col-12">
+                <button type="submit" className="btn btn-primary mb-0">
+                  Submit form
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </section>
